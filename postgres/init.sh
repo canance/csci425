f2e3d5a2fbf843b093caf96ec129a438d55e53fa
Cory Nance <canance@coastal.edu>
2016-01-10 02:02:59 -0500

    Disabled password auth, enable tcp/ip listening in postgreSQL, and set password for postgres db account.

--- postgres/init.sh
+++ postgres/init.sh
@@ -1,10 +1,27 @@
-apt-get update
+# Update
 export DEBIAN_FRONTEND=noninteractive 
+apt-get update
 apt-get upgrade -y
+
+# Install PostgreSQL
 apt-get install postgresql postgresql-contrib vim -y
+
+# Configure SSH
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+systemctl restart sshd.service
+
+# Configure PostgreSQL to listen on localhost:5432
+sed -i 's/#listen_addresses/listen_addresses/g' /etc/postgresql/9.4/main/postgresql.conf
+systemctl restart postgresql.service
+
+# Setup book and extensions
 cd /tmp
-sudo -u postgres psql -c "create extension cube;"
-sudo -u postgres psql -c "create extension tablefunc;"
-sudo -u postgres psql -c "create extension dict_xsyn;"
-sudo -u postgres psql -c "create extension fuzzystrmatch;"
-sudo -u postgres psql -c "create extension pg_trgm;"
+sudo -u postgres createdb book
+sudo -u postgres psql book -c "create extension cube;"
+sudo -u postgres psql book -c "create extension tablefunc;"
+sudo -u postgres psql book -c "create extension dict_xsyn;"
+sudo -u postgres psql book -c "create extension fuzzystrmatch;"
+sudo -u postgres psql book -c "create extension pg_trgm;"
+
+# Set postgres password
+sudo -u postgres psql book -c "ALTER User postgres WITH PASSWORD 'csci425';"
