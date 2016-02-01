8c193eb561ba0b1e75da4eb6685858630257c4b1
Cory Nance <canance@coastal.edu>
2016-02-01 01:08:52 -0500

    Install erlang14 and riak 1.0.2.

--- riak/init.sh
+++ riak/init.sh
@@ -23,50 +23,44 @@ echo "session    required   pam_limits.so" >> /etc/pam.d/common-session
 echo "session    required   pam_limits.so" >> /etc/pam.d/common-session-noninteractive
 
 # Install software
-apt-get install git erlang libssl-dev build-essential libpam0g-dev iptables-persistent ruby -y
+apt-get install git libssl-dev build-essential libpam0g-dev libncurses5-dev ruby ruby-dev -y
 
-# Download and build riak
+# Download, build, and install erlang-R14B02
+cd /tmp
+wget http://erlang.org/download/otp_src_R14B02.tar.gz
+tar xf otp_src_R14B02.tar.gz 
+cd otp_src_R14B02/
+./configure
+make
+make install
+
+# Download and build riak dev
 su - riak << EOSU
 # Clone repo
-git clone https://github.com/basho/riak.git
+git clone -b 1.0.2-release https://github.com/basho/riak.git
 cd riak
 
 # Build
+make
 make devrel
 EOSU
 
 # Download book code
 cd /home/riak
-wget http://media.pragprog.com/titles/rwdata/code/rwdata-code.tgz
+sudo -u riak wget http://media.pragprog.com/titles/rwdata/code/rwdata-code.tgz
 tar xvf rwdata-code.tgz code/riak --strip-components=2
-chown riak:riak *
+sudo -u riak wget http://www.naumik.com/3d/antibiotics.jpg
 rm rwdata-code.tgz
 
 # copy authorized_keys
 mkdir -p /home/riak/.ssh
 cp /root/.ssh/authorized_keys /home/riak/.ssh/authorized_keys
 chown -R riak:riak /home/riak/.ssh
-chmod 700 /riak/home
+chmod 700 /home/riak
 
 # add eXecute bit to dev scripts
 chmod +x {start_,stop_}dev.sh
 
-# iptables rules to match ports used in book
-iptables -t nat -A OUTPUT -p tcp -d 127.0.0.0/8 --dport 8091 -j REDIRECT --to-port 10018
-iptables -t nat -A OUTPUT -p tcp -d 127.0.0.0/8 --dport 8092 -j REDIRECT --to-port 10028
-iptables -t nat -A OUTPUT -p tcp -d 127.0.0.0/8 --dport 8093 -j REDIRECT --to-port 10038
-invoke-rc.d iptables-persistent save
-
-# Download antibotics img
-cd /home/riak
-sudo -u riak wget http://www.naumik.com/3d/antibiotics.jpg
-
 # Install riak and json driver
-gem install riak-client json
-
-# Fix hotels.rb
-# :http-port no longer a valid option, instead use pb_port
-# Keynames are required to be strings
-# https://stackoverflow.com/questions/33170823/ruby-script-hangs-when-storing-riak-object
-sed -i 's/http_port => 8091/pb_port => 10017/g' hotel.rb
-sed -i 's/(current_rooms_block + room)/"#{current_rooms_block + room}"/g' hotel.rb
+gem install riak-client -v 1.0.2
+gem install json
