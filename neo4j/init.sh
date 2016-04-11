98650e52eab7814a20b72839bd677a5fb87eebec
Cory Nance <canance@coastal.edu>
2016-04-11 00:05:58 -0400

    Added Neo4j files.

---
+++ neo4j/init.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+#title           :init.sh
+#description     :This script will provision ubuntu 14.04 with neo4j, download book source code, and setup the environment.
+#author		 :Cory Nance
+#date            :20160409
+#version         :0.1    
+#usage		 :bash init.sh
+
+# Update
+export DEBIAN_FRONTEND=noninteractive
+echo "Updating the system..."
+apt-get update &>/dev/null
+apt-get upgrade -y &>/dev/null
+
+# Configure SSH
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+sed -i 's/#UseLogin no/UseLogin yes/g' /etc/ssh/sshd_config
+service ssh restart &>/dev/null
+
+# Create swapfile
+fallocate -l 2GB /swapfile
+chmod 600 /swapfile
+mkswap /swapfile &>/dev/null
+swapon /swapfile &>/dev/null
+echo /swapfile none swap defaults 0 0 >> /etc/fstab
+
+# Increase ulimit
+echo "* hard nofile 65536" >> /etc/security/limits.conf
+echo "* soft nofile 4096" >> /etc/security/limits.conf
+echo "session    required   pam_limits.so" >> /etc/pam.d/common-session
+echo "session    required   pam_limits.so" >> /etc/pam.d/common-session-noninteractive
+
+# Download book code and supplemental files
+cd /home/neo4j
+echo "Downloading book code..."
+sudo -u neo4j wget http://media.pragprog.com/titles/rwdata/code/rwdata-code.tgz &>/dev/null
+sudo -u neo4j tar xvf rwdata-code.tgz code/neo4j --strip-components=2 &>/dev/null
+rm rwdata-code.tgz
+echo "Downloading performance.tsv..."
+sudo -u neo4j wget http://ci.coastal.edu/~canance/7dbs7wks/performance.tsv &>/dev/null
+
+# copy authorized_keys
+mkdir -p /home/neo4j/.ssh
+cp /root/.ssh/authorized_keys /home/neo4j/.ssh/authorized_keys
+chown -R neo4j:neo4j /home/neo4j/.ssh
+chmod 700 /home/neo4j
+
+echo "Installing Ruby and gems..."
+apt-get install build-essential ruby ruby-dev unzip -y &>/dev/null
+gem install json farady &>/dev/null
+
+# install neo4j
+# debian.neo4j.org
+echo "Installing NEO4J-Enterprise version 1.9.1..."
+wget -O - https://debian.neo4j.org/neotechnology.gpg.key | sudo apt-key add - &>/dev/null
+echo 'deb http://debian.neo4j.org/repo stable/' > /tmp/neo4j.list
+mv /tmp/neo4j.list /etc/apt/sources.list.d
+apt-get update &>/dev/null
+apt-get install neo4j-enterprise=1.9.1 -y &>/dev/null
+
+# install gremlin
+echo "Installing Gremlin..."
+cd /home/neo4j
+sudo -u neo4j wget https://github.com/downloads/tinkerpop/gremlin/gremlin-groovy-2.0.0.zip &>/dev/null
+sudo -u neo4j unzip gremlin-groovy-2.0.0.zip &>/dev/null
+sudo -u neo4j echo alias gremlin=\"bash /home/neo4j/gremlin-groovy-2.0.0/gremlin-groovy.sh\" >> /home/neo4j/.bashrc
+
+# reboot
+echo "A reboot is required ..." 1>&2
+echo "Please allow time for reboot to finish before SSH'ing." 1>&2
+reboot
